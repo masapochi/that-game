b6d28e43230a25a177563e23528bf0c06ed6c27d
まさとし <>
2021-01-08 03:00:59 +0900

    [CLEAN] Move common meta tags in each page to a template file.

--- index.php
+++ index.php
@@ -2,25 +2,9 @@
 <html class="no-js" lang="ja">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>That Game</title>
-  <meta name="description" content="This is &quot;That Game&quot; every one knows.">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="icon" href="dist/images/common/favicon.ico">
-  <link rel="apple-touch-icon" href="dist/images/common/icon.png">
-
+  <?php require_once __DIR__ . '/./templates/meta.php'; ?>
   <link rel="stylesheet" href="dist/css/home.min.css">
-  <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.8.2/css/all.css">
 
-  <meta name="theme-color" content="#adff2f">
 </head>
 
 <body class="home">
